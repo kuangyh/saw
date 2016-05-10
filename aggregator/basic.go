ee5b5ba93a7ae24a636510cb112f3ac184e09852
Yuheng Kuang <kuangyuheng@gmail.com>
2016-05-10 21:30:54 +0000

    Simple stuff: Mean aggregator

--- aggregator/basic.go
+++ aggregator/basic.go
@@ -1,6 +1,8 @@
 package aggregator
 
 import (
+	"math"
+
 	"github.com/kuangyh/saw"
 	"golang.org/x/net/context"
 )
@@ -22,4 +24,50 @@ func (sum *Sum) MergeFrom(other saw.Saw) error {
 
 func (sum *Sum) Result(ctx context.Context) (interface{}, error) {
 	return sum.Current, nil
+}
+
+type MeanState struct {
+	count      Metric
+	sum        Metric
+	sumOfSqure Metric
+}
+
+func (ms *MeanState) Add(metric Metric) {
+	ms.count += Metric(1.0)
+	ms.sum += metric
+	ms.sumOfSqure += metric * metric
+}
+
+func (ms *MeanState) Mean() Metric {
+	if ms.count == 0 {
+		return 0.0
+	}
+	return ms.sum / ms.count
+}
+
+func (ms *MeanState) Stddev() Metric {
+	if ms.count <= 1 {
+		return 0.0
+	}
+	return Metric(math.Sqrt(float64((ms.sumOfSqure - ms.Mean()*ms.sum) / (ms.count - 1.0))))
+}
+
+func (ms *MeanState) Stderr() Metric {
+	if ms.count <= 1 {
+		return 0.0
+	}
+	return ms.Stddev() / Metric(math.Sqrt(float64(ms.count)))
+}
+
+type Mean struct {
+	state MeanState
+}
+
+func (m *Mean) Emit(datum saw.Datum) error {
+	m.state.Add(datum.Value.(Metric))
+	return nil
+}
+
+func (m *Mean) Result() (interface{}, error) {
+	return m.state, nil
 }
